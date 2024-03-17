9818e8baeec49f9d25fef03914f47a13e3b8002b
akashpandya <91130000+akashpandya@users.noreply.github.com>
2024-03-17 09:13:09 -0400

    Added Files

---
+++ app.js
@@ -0,0 +1,42 @@
+const API_KEY = "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"
+const submitIcon = document.querySelector(" #submit-icon ")
+const inputElement = document.querySelector('input')
+const imageSection = document.querySelector('.images-section')
+
+
+const getImages = async () => {
+    const options = {
+        method:"POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            prompt: inputElement.value,
+            n: 4,
+            size: "1024x1024"
+        })
+    }
+
+
+    try{
+        const response = await fetch('https://api.openai.com/v1/images/generations', options)
+        const data = await response.json()
+        console.log(data)
+        data?.data.forEach(imageObject => {
+            const imageContainer = document.createElement('div')
+            imageContainer.classList.add('image-container')
+            const imageElement = document.createElement('img')
+            imageElement.setAttribute('src', imageObject.url)
+            imageContainer.append(imageElement)
+            imageSection.append(imageContainer)
+        });
+    }catch (error){
+        console.error(error)
+    }
+
+}
+
+
+
+submitIcon.addEventListener('click', getImages)
